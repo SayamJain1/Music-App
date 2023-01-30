74f210cdbe1f6561e735ed02cc834605e1e1f092
sanyam jain <sayamjain99300@gmail.com>
2023-01-30 14:41:03 +0530

    debug an error

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import SHAZAM_API from './apikey.js'
+import SHAZAM_API from "./apikey.js";
 
 //  song Container
 let songContainer = document.querySelector(".song-container");
@@ -16,18 +16,18 @@ let prevBtn = document.querySelector("#prev");
 let playBtn = document.querySelector("#play");
 let nextBtn = document.querySelector("#next");
 
-// sliders 
-let volumeSlide = document.querySelector('.volume-slider')
-let seekSlide = document.querySelector('.seek-slider')
+// sliders
+let volumeSlide = document.querySelector(".volume-slider");
+let seekSlide = document.querySelector(".seek-slider");
 
-let durationTime = document.querySelector('.total-duration')
-let currentTimeEl = document.querySelector('.current-time')
+let durationTime = document.querySelector(".total-duration");
+let currentTimeEl = document.querySelector(".current-time");
 
 // Globle
 let currAudio = document.querySelector("audio");
 let songIndex = 0;
 let isPlaying = false;
-songContainer.innerHTML = 'Loading Songs...'
+songContainer.innerHTML = "Loading Songs...";
 
 // Fetching DATA
 
@@ -43,7 +43,7 @@ fetch("https://shazam-core.p.rapidapi.com/v1/charts/world", options)
   .then((response) => response.json())
   .then((response) => {
     if (response) {
-      songContainer.innerText = ''
+      songContainer.innerText = "";
     }
     response.map((songs, i) => {
       let mainDiv = document.createElement("div");
@@ -52,7 +52,7 @@ fetch("https://shazam-core.p.rapidapi.com/v1/charts/world", options)
       let imgDiv = document.createElement("div");
       imgDiv.classList = "img-div";
       let img = document.createElement("img");
-      img.src = songs.images.coverart;
+      img.src = songs?.images?.coverart ? songs.images.coverart : "default.png";
       let h3 = document.createElement("h3");
       h3.classList = "song-name";
       h3.innerText = songs.title;
@@ -76,26 +76,27 @@ fetch("https://shazam-core.p.rapidapi.com/v1/charts/world", options)
       playerSongName.innerHTML = response[songIndex].title;
       play.classList.replace("fa-play-circle", "fa-pause-circle");
     };
-    
+
     // pause
     const pauseMusic = () => {
       isPlaying = false;
-      
+
       currAudio.src = response[songIndex].hub.actions[1].uri;
       currAudio.pause();
       play.classList.replace("fa-pause-circle", "fa-play-circle");
     };
 
     playBtn.addEventListener("click", () => {
+      console.log("clicked");
       if (isPlaying) pauseMusic();
       else playMusic();
     });
 
     const next = () => {
       if (songIndex < response.length - 1) {
-        songIndex++
-      } else songIndex = 0
-      console.log(songIndex)
+        songIndex++;
+      } else songIndex = 0;
+      console.log(songIndex);
 
       currAudio.src = response[songIndex].hub.actions[1].uri;
       playMusic();
@@ -112,42 +113,45 @@ fetch("https://shazam-core.p.rapidapi.com/v1/charts/world", options)
     nextBtn.addEventListener("click", next);
     prevBtn.addEventListener("click", prev);
 
-    volumeSlide.addEventListener('change', () => {
-      currAudio.volume = volumeSlide.value / 100
-    })
+    volumeSlide.addEventListener("change", () => {
+      currAudio.volume = volumeSlide.value / 100;
+    });
 
     // click to song
-    document.querySelectorAll('.song-card').forEach((ele) => (
-      ele.addEventListener('click', () => {
-        musicPlayer.classList.remove('hidden')
-        songIndex = ele.id
+    document.querySelectorAll(".song-card").forEach((ele) =>
+      ele.addEventListener("click", () => {
+        musicPlayer.classList.remove("hidden");
+        songIndex = ele.id;
         currAudio.src = response[songIndex].hub.actions[1].uri;
         playMusic();
       })
-     ))
+    );
 
-    const getTime = (time) => `${Math.floor(time / 60)}:${(`0${Math.floor(time % 60)}`).slice(-2)}`;
-    
-    currAudio.addEventListener('timeupdate', () => {
-      seekSlide.value = parseInt((currAudio.currentTime / currAudio.duration) * 100)
-      currentTimeEl.innerText = getTime(currAudio.currentTime)
+    const getTime = (time) =>
+      `${Math.floor(time / 60)}:${`0${Math.floor(time % 60)}`.slice(-2)}`;
+
+    currAudio.addEventListener("timeupdate", () => {
+      seekSlide.value = parseInt(
+        (currAudio.currentTime / currAudio.duration) * 100
+      );
+      currentTimeEl.innerText = getTime(currAudio.currentTime);
       if (!isNaN(currAudio.duration)) {
-        durationTime.innerText = getTime(currAudio.duration)
+        durationTime.innerText = getTime(currAudio.duration);
       } else {
-        durationTime.innerText = '0:00'
+        durationTime.innerText = "0:00";
       }
-    })
+    });
 
-    currAudio.addEventListener('ended', next)
+    currAudio.addEventListener("ended", next);
 
-    seekSlide.addEventListener('change', () => {
-      currAudio.currentTime = seekSlide.value * currAudio.duration / 100
-    })
-
-    
-  }) 
+    seekSlide.addEventListener("change", () => {
+      currAudio.currentTime = (seekSlide.value * currAudio.duration) / 100;
+    });
+  })
   .catch((err) => {
-    let h2 = document.createElement('h2')
-    h2.innerText = 'Something went wrong please try again!'
-    songContainer.appendChild(h2)
+    console.log(err.message);
+    console.log(err);
+    let h2 = document.createElement("h2");
+    h2.innerText = "Something went wrong please try again!";
+    songContainer.appendChild(h2);
   });
